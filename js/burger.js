0dcd928b298753438f696c7ccccb9ed8e6691c34
mighty-mite <tushinov.ayur@gmail.com>
2022-09-01 12:12:28 +0300

    fix: fix menu, make it a bit prettier

--- js/burger.js
+++ js/burger.js
@@ -7,26 +7,32 @@ const headerText = document.querySelector('.welcome__top-wrapper');
 let isMenuOpen = false;
 
 function showMenu() {
-	if (burgerBtn.style.display === 'block') {
-		menu.style.left = 0;
-		burgerBtn.style.display = 'none';
-		burgerExit.style.display = 'block';
-		headerText.style.opacity = '0';
-		isMenuOpen = true;
-	} else {
+	if (isMenuOpen) {
 		menu.style.left = -150 + '%';
+		document.body.style.overflow = 'auto';
+		document.body.style.height = 'auto';
 		burgerBtn.style.display = 'block';
 		burgerExit.style.display = 'none';
 		headerText.style.opacity = 1;
 		isMenuOpen = false;
+	} else {
+		menu.style.left = 0;
+		document.body.style.overflow = 'hidden';
+		document.body.style.height = '100vh';
+		burgerBtn.style.display = 'none';
+		burgerExit.style.display = 'block';
+		headerText.style.opacity = '0';
+		isMenuOpen = true;
 	}
 }
 
 burger.addEventListener('click', showMenu);
 
 function changeMenu(e) {
-	if (e.target.className == 'header__link') {
-		if (isMenuOpen === true) {
+	if (e.target.classList.contains('header__link')) {
+		if (isMenuOpen) {
+			document.body.style.overflow = 'auto';
+			document.body.style.height = 'auto';
 			menu.style.left = -150 + '%';
 			burgerBtn.style.display = 'block';
 			burgerExit.style.display = 'none';
